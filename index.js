e583e35384465aef2ddb1bbb69969e091c523753
Bianca Aspin <bianca@mj94.net>
2023-01-15 15:36:44 -0800

    Completed lab (Phase 1 review)

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+const employee = {
+    name: "Jim Halpert",
+    streetAddress: "4000 Scranton Avenue",
+};
+
+function updateEmployeeWithKeyAndValue(empObject, key, value) {
+    const modifiedEmployee = {...empObject};
+    modifiedEmployee[key] = value;
+    return modifiedEmployee;
+};
+
+function destructivelyUpdateEmployeeWithKeyAndValue(empObject, key, value) {
+    empObject[key] = value;
+    return empObject;
+};
+
+function deleteFromEmployeeByKey(empObject, key) {
+    const modifiedEmployee = {...empObject};
+    delete modifiedEmployee[key];
+    return modifiedEmployee;
+};
+
+function destructivelyDeleteFromEmployeeByKey(empObject, key) {
+    delete empObject[key];
+    return empObject;
+};
